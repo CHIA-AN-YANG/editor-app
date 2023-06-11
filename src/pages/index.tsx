70fbe7950d11091e842b77c9e6df91d39ee6f9a1
Anna Yang <anna.yang@seidor.com>
2023-06-11 18:12:38 +0800

    change font and layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,20 @@ import PanelLeft from '@components/PanelLeft';
 import PanelRight from '@components/PanelRight';
 import styles from '@styles/main.module.scss';
 import EditorArea from '@components/EditorArea';
+import { Titillium_Web } from 'next/font/google';
+
+const titilliumWeb = Titillium_Web({ 
+  weight: '600',
+  subsets: ['latin'] 
+});
 
 function HomePage() {
   return (
-    
-      <main className={styles.main}>
+      <main className={styles.main+' '+titilliumWeb.className}>
           <PanelLeft></PanelLeft>
           <EditorArea></EditorArea>
           <PanelRight></PanelRight>
-      </main>
+      </main> 
   );
 }
 
